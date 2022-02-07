e6766ae18f6197071d8ec1eb7b3cddb3f07439b7
James Keicher <jamesdkeicher@gmail.com>
2022-02-07 15:06:50 -0800

    chore: fix test

--- src/App.js
+++ src/App.js
@@ -7,16 +7,19 @@ import {
 import BeaniesPage from './BeaniesPage';
 import BeanieDetail from './BeanieDetail';
 
+
 export default function App() {
   return (
     <Router>
       <div className='App'>
         <Switch>
-          <Route>
+          <Route exact path='/' >
+            <BeaniesPage />
             {/* this home page route should list all the beanies */}
           </Route>
-          <Route> 
+          <Route exact path='/detail/:id'> 
             {/* this route should point to a particulat beanie baby by id and render that specific BeanieDetail page */}
+            <BeanieDetail />
           </Route>
         </Switch>
       </div>
--- src/BeaniesList.js
+++ src/BeaniesList.js
@@ -4,5 +4,10 @@ export default function BeaniesList({ beanieBabies }) {
   return (
     <div className='beanies'>
       {/* render out the beanie babies as a list */}
+      {
+        beanieBabies.map((baby, i) => {
+          return <BeanieBaby key={baby.title + i} beanieBaby={baby} />;
+        })
+      }
     </div>);
 }
